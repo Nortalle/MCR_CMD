35caa5b9f9f110865f6fd509ebeea08411dd7afc
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-05-30 15:36:15 +0200

    controller, Game, ICard, Player, SPelle, Unit

--- Java/src/main/java/Controler/Controller.java
+++ Java/src/main/java/Controler/Controller.java
@@ -1,12 +1,57 @@
 package Controler;
 
+import Model.Game;
+import Model.ICard;
+import Model.ICmd;
+import Model.Player;
+
+import java.util.ArrayList;
+
+import static java.lang.Thread.sleep;
+
 public class Controller {
-    private static Controller instance;
 
-    private Controller() {}
+    private Game game;
 
-    public static Controller getInstance() {
-        if(instance == null) instance = new Controller();
-        return instance;
+    public Controller() {
+        game = Game.getInstance();
+    }
+
+    public boolean PlayerHasLost(Player player){
+
+        for(ICard card : player.getCards()){
+            if(card.isAlive()){
+                return false;
+            }
+        }
+        System.out.println("Le joueur " + player + " a perdu");
+        return true;
+    }
+
+    public boolean isGameFinished(){
+
+        return PlayerHasLost(game.getPlayerOne()) || PlayerHasLost(game.getPlayerTwo());
+    }
+
+    public void executeAllCommands(){
+        for(int i = 0; i < Game.nbrActions ; ++i){
+            try {
+                game.getPlayerOne().getActionsList().get(i).execute();
+                sleep(500);
+                game.getPlayerTwo().getActionsList().get(i).execute();
+                sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public boolean addCommandToPlayer(Player player, ICmd cmd){
+        /*
+        On doit faire des tests
+         */
+        player.getActionsList().add(cmd);
+
+        return true;
     }
 }
--- Java/src/main/java/Model/Player.java
+++ Java/src/main/java/Model/Player.java
@@ -6,5 +6,25 @@ import java.util.ArrayList;
  * @author Vincent Guidoux
  */
 public class Player {
+
     private ArrayList<ICard> cards = new ArrayList<ICard>();
+    private ArrayList<ICmd> actionsList = new ArrayList<ICmd>();
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<ICmd> getActionsList() {
+        return actionsList;
+    }
+
+    public ArrayList<ICard> getCards() {
+        return cards;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
 }
