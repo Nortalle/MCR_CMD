c7fe75f000e08f331fdf5828352e417ee29549e3
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-06-11 17:54:50 +0200

    commentaires

--- Java/src/main/java/Model/PlayerLog.java
+++ Java/src/main/java/Model/PlayerLog.java
@@ -5,6 +5,10 @@ import java.awt.*;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
+/**
+ * A player's log which is a Handler which define how the
+ * given information will be displayed
+ */
 public class PlayerLog extends Handler {
     private JTextArea component;
 
@@ -29,6 +33,11 @@ public class PlayerLog extends Handler {
     public void close() throws SecurityException {
     }
 
+    /**
+     * sets the component where the log are going to be written
+     *
+     * @param component : component where the log are going to be written
+     */
     public void setComponent(JTextArea component) {
         this.component = component;
     }
