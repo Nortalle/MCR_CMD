035ea03726f96d1b3a85a8358c5a0a6364e486aa
Unknown <lloysekrug@gmail.com>
2018-06-12 17:02:10 +0200

    Fake Unit commenté

--- Java/src/main/java/Model/units/FakeUnit.java
+++ Java/src/main/java/Model/units/FakeUnit.java
@@ -5,14 +5,22 @@ import Model.Unit;
 
 import java.util.Random;
 
+/**
+ * Class representing a fake unit used as an obstacle
+ */
 public class FakeUnit extends Unit {
-
+    //Path used to chose the image of the fake unit
     private final String path;
 
+    /**
+     * Constructor of the fake unit
+     * @param start, Cell where the fake unit will spawn
+     */
     public FakeUnit(Cell start) {
         super(1, 0, start, "", "");
         Random rand = new Random();
 
+        //The sprite of the obstacle is randomly chosen between two images
         if ((rand.nextInt(2) + 1) % 2 == 0) {
             path = "tree.png";
         } else {
@@ -20,21 +28,16 @@ public class FakeUnit extends Unit {
         }
     }
 
-    public String desctiption() {
-        return "A fake unit, obstructing the way!";
-    }
-
-    public boolean Invoke(Cell c) {
-        return false;
-    }
-
+    //getter of the path to the sprite
     @Override
     public String getPath() {
         return path;
     }
 
+    //Name of the unit
+    @Override
     public String toString() {
-        return "FakeTree";
+        return "FakeUnit";
     }
 
 }
